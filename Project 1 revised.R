358cbc9330d06ccab412aba051df64c7a468d0cc
valenese <rl2633@tc.columbia.edu>
2016-09-20 22:05:01 -0400

    Add files via upload

--- Project 1 revised.R
+++ Project 1 revised.R
@@ -14,12 +14,12 @@ dim(ACS_pusb)
 ACS_pus <- rbind(ACS_pusa, ACS_pusb)
 NACS_pus <- ACS_pus[order(ACS_pus$SERIALNO),] 
 
-ACS_pus_1 = NACS_pus[ ,c("SERIALNO","MAR","MARHD","MARHD","MARHM","MARHT","MARHW","MARHYP","OCCP"
+ACS_pus_1 = NACS_pus[ ,c("SERIALNO","PWGTP","MAR","MARHD","MARHD","MARHM","MARHT","MARHW","MARHYP","OCCP"
                          ,"WKW","POWSP","PINCP","SCHL","FOD1P","AGEP","RAC1P","RAC2P","POBP",
                          "FMARP","FMARHDP","FMARHMP","FMARHTP","FMARHWP","FMARHYP","FESRP",
                          "FOCCP","FWKWP","FPOWSP","FPINCP","FSCHLP","FAGEP","FPOBP")]
 dim(ACS_pus_1)
-ACS_pus_2 <- subset(ACS_pus_1, AGEP >= 21 & AGEP <=30 & MAR != 2)
+ACS_pus_2 <- subset(ACS_pus_1, AGEP >= 21 & AGEP <= 30 & MAR != 2)
 dim(ACS_pus_2)
 ACS_pus_2[1:50,]
 
@@ -77,50 +77,70 @@ for (i in 1:9){
 }
 ACS_pus_2$Race[1:500]      
 
+########## Historgram for independent variables############ 
+# variables: Occupation + Length.of.Work + Personal Income + Education + Age + Race 
+ggplot(ACS_pus_2, aes(Occupation, ..count..,weight=Weight)) + 
+  geom_bar(aes(fill = ..count..))+
+  xlab("Occupation")+
+  ylab("Percentage")
+# Length of work
+ggplot(ACS_pus_2, aes(Length.of.Work, ..count..,weight=Weight)) + 
+  geom_bar(aes(fill = ..count..))+
+  xlab("Length of Work")+
+  ylab("Percentage")
+# Education
+ggplot(ACS_pus_2, aes(SCHL_code, ..count..,weight=Weight)) + 
+  geom_bar(aes(fill = ..count..))+
+  xlab("Education")+
+  ylab("Percentage")
+# Age
+ggplot(ACS_pus_2, aes(AGEP, ..count..,weight=Weight)) + 
+  geom_bar(aes(fill = ..count..))+
+  xlab("Age")+
+  ylab("Percentage")
+#Race
+ggplot(ACS_pus_2, aes(Race, ..count..,weight=Weight)) + 
+  geom_bar(aes(fill = ..count..))+
+  xlab("Race")+
+  ylab("Percentage")
+# Personal Income
+ggplot((ACS_pus_2), aes(PINCP, ..count..,weight=Weight))+
+  geom_histogram(aes(fill = ..count..),binwidth = 50000)+
+  xlab("Persobal Income")+
+  ylab("Percentage")
 
+#More clear visualization for income only: 
 
-## logistic regression 
-class_model = glm(MAR_New ~ Occupation+Length.of.Work+PINCP+SCHL_code+AGEP+Race, data = na.omit(ACS_pus_2),family = "binomial")
-head(fitted.values(class_model))
-summary(class_model)
-# training error
-error = abs(round(fitted.values(class_model))-na.omit(ACS_pus_2)$MAR_New)
-sum(error)      
+# Personal Income
+ggplot((ACS_pus_2), aes(PINCP, ..count..,weight=Weight))+
+  geom_histogram(aes(fill = ..count..))+
+  scale_x_sqrt() +
+  xlab("Persobal Income")+
+  ylab("Density")
 
-## CV Logistic Regression 
-glmcv = cv.glm(na.omit(ACS_pus_2), class_model, K=5)
-glmcv$delta
-
-#####logistic regression without occupation
-class_model2 = glm(MAR_New ~ Length.of.Work+PINCP+SCHL_code+AGEP+Race, data = na.omit(ACS_pus_2),family = "binomial")
-head(fitted.values(class_model2))
-summary(class_model2)
-# training error
-error2 = abs(round(fitted.values(class_model2))-na.omit(ACS_pus_2)$MAR_New)
-sum(error2) 
 
 
 ################# Visualization ################
 Marriage.Status = as.factor(ACS_pus_2$MAR_New)
-ggplot(ACS_pus_2, aes(Occupation,fill = Marriage.Status)) + 
+ggplot(ACS_pus_2, aes(Occupation,fill = Marriage.Status,weight=Weight)) + 
   geom_bar(position = "fill") +
   xlab("Occupation") +
   ylab("Percentage of Marriage Status") + 
   scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))  
 
-ggplot(ACS_pus_2, aes(Length.of.Work,fill = Marriage.Status)) + 
+ggplot(ACS_pus_2, aes(Length.of.Work,fill = Marriage.Status,weight=Weight)) + 
   geom_bar(position = "fill")+
   xlab("Length of Work") +
   ylab("Percentage of Marriage Status") + 
   scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))  
 
-ggplot(ACS_pus_2, aes(SCHL_code,fill = Marriage.Status)) + 
+ggplot(ACS_pus_2, aes(SCHL_code,fill = Marriage.Status,weight=Weight)) + 
   geom_bar(position = "fill")+
   xlab("Education") +
   ylab("Percentage of Marriage Status") + 
   scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))  
 
-ggplot(ACS_pus_2, aes(Race,fill = Marriage.Status)) + 
+ggplot(ACS_pus_2, aes(Race,fill = Marriage.Status,weight=Weight)) + 
   geom_bar(position = "fill")+
   xlab("Race") +
   ylab("Percentage of Marriage Status") + 
@@ -132,63 +152,56 @@ ggplot(ACS_pus_2, aes(AGEP,fill = Marriage.Status)) +
   ylab("Percentage of Marriage Status") + 
   scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))  
 
-ggplot(ACS_pus_2, aes(PINCP,fill = Marriage.Status)) + 
+ggplot(ACS_pus_2, aes(PINCP,fill = Marriage.Status,weight=Weight)) + 
   geom_histogram(position = "fill",binwidth = 130000)+
   xlab("Personal Income") +
   ylab("Percentage of Marriage Status") + 
-  scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))   
+  scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))  
+
+ggplot(ACS_pus_2, aes(Race,fill = Marriage.Status,weight=Weight)) + 
+  geom_bar(position = "fill")+
+  xlab("Race") +
+  ylab("Percentage of Marriage Status") + 
+  scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))  +
+  # third dimension eg:education:
+  facet_grid ( . ~ SCHL_code ) +
+  # rotate x axis, eg:angle = 45 or 90 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(ACS_pus_2, aes(AGEP,fill = Marriage.Status,weight=Weight)) + 
+  geom_bar(position = "fill")+
+  xlab("Race") +
+  ylab("Percentage of Marriage Status") + 
+  scale_fill_manual("Marriage Status",values = alpha( c("firebrick", "dodgerblue4"), 1))  +
+  # third dimension eg:education:
+  facet_grid ( . ~ SCHL_code ) +
+  # rotate x axis, eg:angle = 45 or 90 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+############## logistic regression############### 
+ACS_pus_2$Weight = ACS_pus_2$PWGTP/sum(ACS_pus_2$PWGTP)
+class_model = glm(MAR_New ~ Occupation+Length.of.Work+PINCP+SCHL_code+AGEP+Race, data = na.omit(ACS_pus_2),
+                  family = "binomial",weights = Weight)
+head(fitted.values(class_model))
+summary(class_model)
+# training error
+error = abs(round(fitted.values(class_model))-na.omit(ACS_pus_2)$MAR_New)
+sum(error)      
+
+## CV Logistic Regression 
+glmcv = cv.glm(na.omit(ACS_pus_2), class_model, K=5)
+glmcv$delta
+
+########logistic regression without occupation#############
+class_model2 = glm(MAR_New ~ Length.of.Work+PINCP+SCHL_code+AGEP+Race, data = na.omit(ACS_pus_2),
+                   family = "binomial",weights = Weight)
+head(fitted.values(class_model2))
+summary(class_model2)
+# training error
+error2 = abs(round(fitted.values(class_model2))-na.omit(ACS_pus_2)$MAR_New)
+sum(error2)
 
 
 
-## Historgram for independent variables 
-# variables: Occupation + Length.of.Work + Personal Income + Education + Age + Race 
-# Occupency
-ggplot(na.omit(ACS_pus_2), aes(Occupation, ..count..)) + 
-  geom_bar(aes(fill = ..count..))+
-  xlab("Occupation")
-# Length of work
-ggplot(na.omit(ACS_pus_2), aes(Length.of.Work, ..count..)) + 
-  geom_bar(aes(fill = ..count..))+
-  xlab("Length of Work")
-# Education
-ggplot(ACS_pus_2, aes(SCHL_code, ..count..)) + 
-  geom_bar(aes(fill = ..count..))+
-  xlab("Education")
-# Age
-ggplot(ACS_pus_2, aes(AGEP, ..count..)) + 
-  geom_bar(aes(fill = ..count..))+
-  xlab("Age")
-#Race
-ggplot(ACS_pus_2, aes(Race, ..count..)) + 
-  geom_bar(aes(fill = ..count..))+
-  xlab("Race")
-# Personal Income
-ggplot((ACS_pus_2), aes(PINCP, ..count..))+
-  geom_histogram(aes(fill = ..count..),binwidth = 50000)+
-  xlab("Persobal Income")
 
-
-
-
-
-
-
-
-
-
-################################ Reference #############################
-## Test of variabe FES 
-ACS_pus_1 = NACS_pus[ ,"FES"]
-head(NACS_pus$FES)               
-
-
-## used variables
-"SERIALNO","MAR","MARHD","MARHD","MARHM","MARHT","MARHW","MARHYP","OCCP",
-"WKW","POWSP","PINCP","FINCP","SCHL","FOD1P","AGEP","RAC1P","RAC2P","POBP",
-"FSERIALNOP","FMARP","FFESP","FHHTP","FSSMCP","FMARHDP","FMARHMP","FMARHTP",
-"FMARHWP","FMARHYPP","FMSPP","FESRP","FOCCPP","FWKWP","FPOWSPP","FPINCPP",
-"FFINCPP","FSCHLP","FFOD1PP","FAGEPP","FRAC1PP","FRAC2PP","FPOBPP"
-## out of senariro
-"FES"
-## unused variables
-,"SSMC","HHT","MSP","ESR"
